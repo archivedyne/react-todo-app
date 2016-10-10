d5e385bcf38912fdc3a2883ac55a9114a43c7ad8
mgoldchild <mgoldchild@gmail.com>
2016-10-10 11:28:35 +0800

    build⌘を簡略化

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var requireDir = require('require-dir');
 var runSequence = require('run-sequence');
 
 requireDir('./gulp_tasks', {recurse: true});
+gulp.task('prod', ['css']);
 // gulp.task('dinamyc:compile', ['hbs:compile', 'dynamic:coffee:compile', 'dynamic:less:compile']);
 // gulp.task('dinamyc:compile', ['hbs:compile', 'dynamic:coffee:compile', 'dynamic:less:compile']);
 // gulp.task('static:compile', ['static:coffee:compile', 'static:less:compile', 'font:compile']);
